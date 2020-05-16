d3652af7e6ae07cf08888dfda63dc801f88b4680
Leon de Almeida <leoncustodio12@hotmail.com>
2020-05-16 09:09:25 -0400

    Use denotrain server

--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 import * as rimu from "https://raw.github.com/srackham/rimu/master/mod.ts";
+import { Application, Router } from "https://deno.land/x/denotrain@v0.4.3/mod.ts";
 
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
 const index = new TextDecoder("utf-8").decode(await Deno.readFile("./src/index.md" as string));
-for await (const req of s) {
-  req.respond({ 
-    body: rimu.render(index) 
-  });
-}
+const indexHTML = rimu.render(index);
+
+const app = new Application({port: 3001});
+
+app.use((ctx) => {
+  ctx.cookies["content-type"] = "html";
+  return;
+});
+
+app.get("/", (ctx) => {
+  ctx.res.setMimeType('text/html')
+  return indexHTML; 
+});
+
+await app.run();
